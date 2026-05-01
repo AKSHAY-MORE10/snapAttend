5c005bafd0da33568c87f2988de5b87bd33af718
AKSHAY-MORE10 <akshaybapumore10@gmail.com>
2026-05-01 21:53:07 +0530

    full porject done jjust deployment remaining

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -45,8 +45,8 @@ def style_base_layout():
         @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
-        # #MainMenu, footer, header {
-        #     visibility: hidden;
+        #MainMenu, footer, header {
+            visibility: hidden;
         }
 
         .block-container {
